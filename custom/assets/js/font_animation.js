8bdabe4c9bb1acbf215d0dc8c718ed1175c3849e
Usama <nhusain34@gmail.com>
2023-06-09 23:02:41 +0530

    Pricing Section added to all Services pages, home page text animation added, Menu made fixed

--- custom/assets/js/font_animation.js
+++ custom/assets/js/font_animation.js
@@ -1,6 +1,32 @@
-const words=["Success","Services","Business"];
-
-var element = document.getElementById('banner-title-line');
-element.innerText="";
-
-
+animate_words_characters();
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function animate_words_characters(){
+    const words=["Success","Services","Business"];
+    var element = document.getElementById('banner-title-line');
+    element.innerText="";
+    const total_words = words.length;
+    let word_index = 0;
+    while(true){
+        let char_index = 0;
+        let word = words[word_index];
+        let word_length = word.length;
+        let text = "";
+        while(char_index < word_length){
+            text += word[char_index];
+            element.innerText=text;
+            await sleep(100);
+            char_index++;
+        }
+        await sleep(500);
+        char_index-=1;
+        while(char_index >= 0){
+            text = text.slice(0, char_index);            
+            element.innerText=text;            
+            await sleep(100);
+            char_index--;
+        }
+        word_index=(word_index+1)%total_words;
+    }
+}
